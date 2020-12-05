b76105146c2af0b9c13afbeeca6af37337534f7b
Victor Goubet <victor.goubet@edu.devinci.fr>
2020-12-05 19:09:27 +0100

    Création du projet de tests unitaires

---
+++ Solution/Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //chloé pu du cul
+        }
+    }
+}

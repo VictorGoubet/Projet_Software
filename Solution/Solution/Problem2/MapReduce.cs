9080b9b650d19bee92d05ce2ed00e60d6d988654
Victor Goubet <victor.goubet@edu.devinci.fr>
2020-12-15 16:17:15 +0100

    Debut du mapreduce

---
+++ Solution/Solution/Problem2/MapReduce.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Problem2
+{
+    class MapReduce<K1, V1, K2, V2, K3, V3>
+    {
+
+        public delegate IEnumerable<KeyValuePair<K2, V2>> map_function(K1 key, V1 val);
+        public delegate KeyValuePair<K3, V3> reduce_function(K2 key, IEnumerable<V2> val);
+
+
+
+        private map_function map;
+        private reduce_function reduce;
+
+        public MapReduce(map_function map, reduce_function reduce)
+        {
+            this.map = map;
+            this.reduce = reduce;
+        }
+
+        private Dictionary<K2, List<V2>> Shuffling(List<IEnumerable<KeyValuePair<K2, V2>>> res_map)
+        {
+            Dictionary<K2, List<V2>> res_shuffling = new Dictionary<K2, List<V2>>();
+            foreach(IEnumerable<KeyValuePair<K2, V2>> x in res_map)
+            {
+                foreach(KeyValuePair<K2, V2> y in x)
+                {
+                    if (res_shuffling.ContainsKey(y.Key))
+                    {
+                        res_shuffling[y.Key].Add(y.Value);
+                    }
+                    else
+                    {
+                        List<V2> temp = new List<V2>();
+                        temp.Add(y.Value);
+                        res_shuffling.Add(y.Key, temp);
+                    }  
+                }
+            }
+            return res_shuffling;
+        }
+
+        private List<IEnumerable<KeyValuePair<K2, V2>>> apply_all_map(IEnumerable<KeyValuePair<K1, V1>> input)
+        {
+            List<IEnumerable<KeyValuePair<K2, V2>>> res_map = new List<IEnumerable<KeyValuePair<K2, V2>>>();
+            foreach (KeyValuePair<K1, V1> res_splitting in input)
+            {
+                res_map.Add(this.map(res_splitting.Key, res_splitting.Value));
+            }
+            return res_map;
+        }
+
+        private List<KeyValuePair<K3, V3>> apply_all_reduce(Dictionary<K2, List<V2>> res_shuffling)
+        {
+            List<KeyValuePair<K3, V3>> res = new List<KeyValuePair<K3, V3>>();
+            foreach(KeyValuePair<K2, List<V2>> x in res_shuffling)
+            {
+                res.Add(this.reduce(x.Key, x.Value));
+            }
+            return res;
+        }
+        
+
+        public IEnumerable<KeyValuePair<K3, V3>> get_result(IEnumerable<KeyValuePair<K1, V1>> input)
+        {
+
+            List<IEnumerable<KeyValuePair<K2, V2>>> res_map = this.apply_all_map(input);
+            Dictionary<K2, List<V2>> res_shuffling = this.Shuffling(res_map);
+            List<KeyValuePair<K3, V3>> res = this.apply_all_reduce(res_shuffling);
+            return res;
+
+        }
+
+
+
+
+    }
+}
--- Solution/Solution/Program.cs
+++ Solution/Solution/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Solution.Problem1;
+using Solution.Problem2;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Solution
 {
@@ -9,7 +12,7 @@ namespace Solution
         public static void Problem1()
         {
             #region Creation of the queue
-            Queue<int> myqueue = new Queue<int>();
+            Solution.Problem1.Queue<int> myqueue = new Solution.Problem1.Queue<int>();
 
             myqueue.enqueue(5);
             myqueue.enqueue(10);
@@ -46,7 +49,7 @@ namespace Solution
             #region Equals
             Console.WriteLine("----- EQUALS -----");
 
-            Queue<int> myqueue2 = new Queue<int>();
+            Solution.Problem1.Queue<int> myqueue2 = new Solution.Problem1.Queue<int>();
             myqueue2.enqueue(5);
             myqueue2.enqueue(10);
 
@@ -76,6 +79,77 @@ namespace Solution
         }
 
 
+        public static void Problem2()
+        {
+            Dictionary<string, string> inputData = new Dictionary<string, string> { 
+                ["firstDocument"] = "hello hello je suis pierrick et je suis la pour vous faire rigoler",
+                ["secondDocument"] = "je suis encore là!"};
+
+            IEnumerable<int> Reduce2(string key, IEnumerable<int> values)
+            {
+                int sum = 0;
+                foreach (int value in values)
+                {
+                    sum += value;
+                }
+                return new int[1] { sum };
+            }
+
+            IList<KeyValuePair<string, int>> Map2(string key, string value)
+            {
+                List<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>>();
+                foreach (var word in value.Split(' '))
+                {
+                    result.Add(new KeyValuePair<string, int>(word, 1));
+                }
+                return result;
+            }
+
+
+            MapReduce2<string, string, string, int, int> master = new MapReduce2<string, string, string, int, int>(Map2, Reduce2);
+            var result = master.Execute(inputData).ToDictionary(key => key.Key, v => v.Value);
+
+            foreach(KeyValuePair<string, int> pair in result)
+            {
+                Console.WriteLine(pair.Key+": "+pair.Value);
+            }
+
+
+
+
+
+
+
+            KeyValuePair<string, int> Reduce(string key, IEnumerable<int> values)
+            {
+                int sum = 0;
+                foreach (int value in values)
+                {
+                    sum += value;
+                }
+                return new KeyValuePair<string, int>(key, sum);
+            }
+
+            IList<KeyValuePair<string, int>> Map(string key, string value)
+            {
+                List<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>>();
+                foreach (var word in value.Split(' '))
+                {
+                    result.Add(new KeyValuePair<string, int>(word, 1));
+                }
+                return result;
+            }
+
+            MapReduce<string, string, string, int, string, int> mapreduce = new MapReduce<string, string, string, int, string, int>(Map, Reduce);
+            IEnumerable<KeyValuePair<string, int>> res = mapreduce.get_result(inputData);
+
+            foreach (KeyValuePair<string, int> pair in res)
+            {
+                Console.WriteLine(pair.Key + ": " + pair.Value);
+            }
+
+        }
+
 
 
 
@@ -83,7 +157,8 @@ namespace Solution
 
         static void Main(string[] args)
         {
-            Problem1();
+            //Problem1();
+            Problem2();
         }
     }
 }
